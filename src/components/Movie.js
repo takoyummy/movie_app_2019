c985559fcad72e4f9bde8ebfc2ed70af8573a87f
takoyummy <40001921+takoyummy@users.noreply.github.com>
2021-04-07 17:46:43 +0900

    update gh-pages

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {Link } from "react-router-dom"
 import PropTypes from "prop-types";
 import "./Movie.css"
 
-function Movie({ year, title, summary, poster,genres }) {
-  return( <div className="movie">
+function Movie({ id, year, title, summary, poster,genres }) {
+  return( 
+  <Link to={{
+    pathname: `/movie/${id}`,
+    state:{
+      year,
+      title,
+      summary,
+      poster,
+      genres
+    }
+  }}>
+  <div className="movie">
       <img src={poster} alt={title} title={title} />
     <div className="movie_data">
       <h3 className="movie__title">{title}</h3>
@@ -19,6 +31,7 @@ function Movie({ year, title, summary, poster,genres }) {
       
       </div>
   </div>
+  </Link>
   );
 }
 
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./About.css"
+
+function About(props){
+    console.log(props)
+    return <span>About this page: I built it beacuse I love movies.</span>
+}
+
+export default About
